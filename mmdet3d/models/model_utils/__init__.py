d018cd304088e17b2ee9b83c3c7778503e014754
rui <919304134@qq.com>
2023-02-15 10:01:31 +0800

    add PillarAttention

--- mmdet3d/models/model_utils/__init__.py
+++ mmdet3d/models/model_utils/__init__.py
@@ -1,6 +1,6 @@
 # Copyright (c) OpenMMLab. All rights reserved.
 from .edge_fusion_module import EdgeFusionModule
-from .transformer import GroupFree3DMHA
+from .transformer import GroupFree3DMHA, PillarAttention
 from .vote_module import VoteModule
 
-__all__ = ['VoteModule', 'GroupFree3DMHA', 'EdgeFusionModule']
+__all__ = ['VoteModule', 'GroupFree3DMHA', 'EdgeFusionModule', 'PillarAttention']
--- mmdet3d/models/model_utils/transformer.py
+++ mmdet3d/models/model_utils/transformer.py
@@ -1,22 +1,22 @@
 # Copyright (c) OpenMMLab. All rights reserved.
 from mmcv.cnn.bricks.registry import ATTENTION
-from mmcv.cnn.bricks.transformer import POSITIONAL_ENCODING, MultiheadAttention
+from mmcv.cnn.bricks.transformer import POSITIONAL_ENCODING, MultiheadAttention, FFN
 from torch import nn as nn
 
 
 @ATTENTION.register_module()
 class GroupFree3DMHA(MultiheadAttention):
-    """A warpper for torch.nn.MultiheadAttention for GroupFree3D.
+    """A wrapper for torch.nn.MultiheadAttention for GroupFree3D.
 
     This module implements MultiheadAttention with identity connection,
     and positional encoding used in DETR is also passed as input.
 
     Args:
-        embed_dims (int): The embedding dimension.
+        embed_dims (int): The embedding dimension. 编码维度
         num_heads (int): Parallel attention heads. Same as
-            `nn.MultiheadAttention`.
+            `nn.MultiheadAttention`. 多头注意力
         attn_drop (float, optional): A Dropout layer on attn_output_weights.
-            Defaults to 0.0.
+            Defaults to 0.0. 注意力输出权重的dropout
         proj_drop (float, optional): A Dropout layer. Defaults to 0.0.
         dropout_layer (obj:`ConfigDict`, optional): The dropout_layer used
             when adding the shortcut.
@@ -122,7 +122,8 @@ class ConvBNPositionalEncoding(nn.Module):
         super().__init__()
         self.position_embedding_head = nn.Sequential(
             nn.Conv1d(input_channel, num_pos_feats, kernel_size=1),
-            nn.BatchNorm1d(num_pos_feats), nn.ReLU(inplace=True),
+            nn.BatchNorm1d(num_pos_feats),
+            nn.ReLU(inplace=True),
             nn.Conv1d(num_pos_feats, num_pos_feats, kernel_size=1))
 
     def forward(self, xyz):
@@ -137,3 +138,88 @@ class ConvBNPositionalEncoding(nn.Module):
         xyz = xyz.permute(0, 2, 1)
         position_embedding = self.position_embedding_head(xyz)
         return position_embedding
+
+
+@ATTENTION.register_module()
+class PillarAttention(MultiheadAttention):
+    def __init__(self,
+                 embed_dims,  # default288
+                 num_heads,
+                 attn_drop=0.,
+                 proj_drop=0.,
+                 dropout_layer=None,
+                 init_cfg=None,
+                 batch_first=True,
+                 **kwargs):
+        super().__init__(embed_dims, num_heads, attn_drop, proj_drop,
+                         dropout_layer, init_cfg, batch_first, **kwargs)
+
+    def forward(self,
+                query,
+                key=None,
+                value=None,
+                identity=None,
+                query_pos=True,
+                key_pos=True,
+                attn_mask=None,
+                key_padding_mask=None,
+                **kwargs):
+        # 判断是否为自注意力
+        if key is None:
+            key = query
+        if value is None:
+            value = key
+        # 残差结构
+        if identity is None:
+            identity = query
+        # 位置编码
+        pos_encoding = ConvBNPositionalEncoding(3, query.shape[-1])
+        query_xyz = query[:, :, :3]  # 选取坐标
+        key_xyz = key[:, :, :3]
+        query_posencode = pos_encoding(query_xyz)
+        key_posencode = pos_encoding(key_xyz)
+        # key和query使用同一个位置编码
+        # if key_pos is None:
+        #     if query_pos is not None:
+        #         # use query_pos if key_pos is not available
+        #         if query_pos.shape == key.shape:
+        #             key_pos = query_pos
+        #         else:
+        #             warnings.warn(f'position encoding of key is'
+        #                           f'missing in {self.__class__.__name__}.')
+        # key和query使用各自位置编码
+        if query_pos:
+            if self.batch_first:
+                query = query + query_posencode.transpose(1,2) # 特征向量+位置编码
+            else:
+                query = query + query_posencode
+        if key_pos:
+            if self.batch_first:
+                key = key + key_posencode.transpose(1,2)
+            else:
+                key = key + key_posencode
+        # 交换bs和number位置
+        if self.batch_first:
+            query = query.transpose(0, 1)
+            key = key.transpose(0, 1)
+            value = value.transpose(0, 1)
+        # 放入attention块中进行计算
+        out = self.attn(
+            query=query,
+            key=key,
+            value=value,
+            attn_mask=attn_mask,
+            key_padding_mask=key_padding_mask)[0]  # 列表第一个位置是features，第二个位置是weights
+        # 恢复bs和number位置
+        if self.batch_first:
+            out = out.transpose(0, 1)
+
+        attn_out = identity + self.dropout_layer(self.proj_drop(out))  # 残差结构
+        # 创建FFN层
+        ffn = FFN(embed_dims=attn_out.shape[-1], feedforward_channels=1024, num_fcs=2,ffn_drop=0.,
+                  dropout_layer=None, add_identity=True)  # True使用残差
+        attn_out = ffn(attn_out)
+
+        return attn_out
+
+
