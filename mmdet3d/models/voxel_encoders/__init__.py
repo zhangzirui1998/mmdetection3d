25ce4ff9e88ea97c961b6442acc2653b305babeb
rui <919304134@qq.com>
2023-02-15 10:04:00 +0800

    add AttnPFN

--- mmdet3d/models/voxel_encoders/__init__.py
+++ mmdet3d/models/voxel_encoders/__init__.py
@@ -1,8 +1,8 @@
 # Copyright (c) OpenMMLab. All rights reserved.
-from .pillar_encoder import DynamicPillarFeatureNet, PillarFeatureNet
+from .pillar_encoder import DynamicPillarFeatureNet, PillarFeatureNet, AttnPFN
 from .voxel_encoder import DynamicSimpleVFE, DynamicVFE, HardSimpleVFE, HardVFE
 
 __all__ = [
     'PillarFeatureNet', 'DynamicPillarFeatureNet', 'HardVFE', 'DynamicVFE',
-    'HardSimpleVFE', 'DynamicSimpleVFE'
+    'HardSimpleVFE', 'DynamicSimpleVFE', 'AttnPFN'
 ]
