350c5f4ee48a0d958eb369bde255cd2012d58057
rui <919304134@qq.com>
2023-02-20 16:56:59 +0800

    与源文件参数相同

--- configs/_base_/schedules/cyclic_40e.py
+++ configs/_base_/schedules/cyclic_40e.py
@@ -3,7 +3,7 @@
 # The learning rate set in the cyclic schedule is the initial learning rate
 # rather than the max learning rate. Since the target_ratio is (10, 1e-4),
 # the learning rate will change from 0.0018 to 0.018, than go to 0.0018*1e-4
-lr = 0.0001
+lr = 0.0018  # 在模型配置文件中重写
 # The optimizer follows the setting in SECOND.Pytorch, but here we use
 # the official AdamW optimizer implemented by PyTorch.
 optimizer = dict(type='AdamW', lr=lr, betas=(0.95, 0.99), weight_decay=0.01)
@@ -28,4 +28,4 @@ momentum_config = dict(
 # Although the max_epochs is 40, this schedule is usually used we
 # RepeatDataset with repeat ratio N, thus the actual max epoch
 # number could be Nx40
-runner = dict(type='EpochBasedRunner', max_epochs=40)
+runner = dict(type='EpochBasedRunner', max_epochs=40)  # 在模型配置文件中重写
