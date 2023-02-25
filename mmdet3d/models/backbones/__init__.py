a8e48b20cbc909c1c2f906b1f2a513f0cdd785be
rui <919304134@qq.com>
2023-02-25 15:57:35 +0800

    add csp_second

--- mmdet3d/models/backbones/__init__.py
+++ mmdet3d/models/backbones/__init__.py
@@ -8,10 +8,11 @@ from .nostem_regnet import NoStemRegNet
 from .pointnet2_sa_msg import PointNet2SAMSG
 from .pointnet2_sa_ssg import PointNet2SASSG
 from .second import SECOND
+from .csp_second import RCSECOND
 
 # __all__变量控制import*传入的内容
 __all__ = [
     'ResNet', 'ResNetV1d', 'ResNeXt', 'SSDVGG', 'HRNet', 'NoStemRegNet',
     'SECOND', 'DGCNNBackbone', 'PointNet2SASSG', 'PointNet2SAMSG',
-    'MultiBackbone', 'DLANet', 'MinkResNet'
+    'MultiBackbone', 'DLANet', 'MinkResNet', 'RCSECOND'
 ]
