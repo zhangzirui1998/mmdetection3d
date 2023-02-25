6cb9b911d9686d1312f2143f4322d0bce15af8b9
rui <919304134@qq.com>
2023-02-25 17:08:23 +0800

    仅仅保留kitti格式，其他格式注释

--- tools/create_data.py
+++ tools/create_data.py
@@ -2,9 +2,9 @@
 import argparse
 from os import path as osp
 
-from tools.data_converter import indoor_converter as indoor
+#from tools.data_converter import indoor_converter as indoor
 from tools.data_converter import kitti_converter as kitti
-from tools.data_converter import lyft_converter as lyft_converter
+#from tools.data_converter import lyft_converter as lyft_converter
 from tools.data_converter import nuscenes_converter as nuscenes_converter
 from tools.data_converter.create_gt_database import (
     GTDatabaseCreater, create_groundtruth_database)
@@ -90,65 +90,65 @@ def nuscenes_data_prep(root_path,
                                 f'{out_dir}/{info_prefix}_infos_train.pkl')
 
 
-def lyft_data_prep(root_path, info_prefix, version, max_sweeps=10):
-    """Prepare data related to Lyft dataset.
-
-    Related data consists of '.pkl' files recording basic infos.
-    Although the ground truth database and 2D annotations are not used in
-    Lyft, it can also be generated like nuScenes.
-
-    Args:
-        root_path (str): Path of dataset root.
-        info_prefix (str): The prefix of info filenames.
-        version (str): Dataset version.
-        max_sweeps (int, optional): Number of input consecutive frames.
-            Defaults to 10.
-    """
-    lyft_converter.create_lyft_infos(
-        root_path, info_prefix, version=version, max_sweeps=max_sweeps)
-
-
-def scannet_data_prep(root_path, info_prefix, out_dir, workers):
-    """Prepare the info file for scannet dataset.
-
-    Args:
-        root_path (str): Path of dataset root.
-        info_prefix (str): The prefix of info filenames.
-        out_dir (str): Output directory of the generated info file.
-        workers (int): Number of threads to be used.
-    """
-    indoor.create_indoor_info_file(
-        root_path, info_prefix, out_dir, workers=workers)
+# def lyft_data_prep(root_path, info_prefix, version, max_sweeps=10):
+#     """Prepare data related to Lyft dataset.
+#
+#     Related data consists of '.pkl' files recording basic infos.
+#     Although the ground truth database and 2D annotations are not used in
+#     Lyft, it can also be generated like nuScenes.
+#
+#     Args:
+#         root_path (str): Path of dataset root.
+#         info_prefix (str): The prefix of info filenames.
+#         version (str): Dataset version.
+#         max_sweeps (int, optional): Number of input consecutive frames.
+#             Defaults to 10.
+#     """
+#     lyft_converter.create_lyft_infos(
+#         root_path, info_prefix, version=version, max_sweeps=max_sweeps)
 
 
-def s3dis_data_prep(root_path, info_prefix, out_dir, workers):
-    """Prepare the info file for s3dis dataset.
+# def scannet_data_prep(root_path, info_prefix, out_dir, workers):
+#     """Prepare the info file for scannet dataset.
+#
+#     Args:
+#         root_path (str): Path of dataset root.
+#         info_prefix (str): The prefix of info filenames.
+#         out_dir (str): Output directory of the generated info file.
+#         workers (int): Number of threads to be used.
+#     """
+#     indoor.create_indoor_info_file(
+#         root_path, info_prefix, out_dir, workers=workers)
 
-    Args:
-        root_path (str): Path of dataset root.
-        info_prefix (str): The prefix of info filenames.
-        out_dir (str): Output directory of the generated info file.
-        workers (int): Number of threads to be used.
-    """
-    indoor.create_indoor_info_file(
-        root_path, info_prefix, out_dir, workers=workers)
+
+# def s3dis_data_prep(root_path, info_prefix, out_dir, workers):
+#     """Prepare the info file for s3dis dataset.
+#
+#     Args:
+#         root_path (str): Path of dataset root.
+#         info_prefix (str): The prefix of info filenames.
+#         out_dir (str): Output directory of the generated info file.
+#         workers (int): Number of threads to be used.
+#     """
+#     indoor.create_indoor_info_file(
+#         root_path, info_prefix, out_dir, workers=workers)
 
 
-def sunrgbd_data_prep(root_path, info_prefix, out_dir, workers, num_points):
-    """Prepare the info file for sunrgbd dataset.
-
-    Args:
-        root_path (str): Path of dataset root.
-        info_prefix (str): The prefix of info filenames.
-        out_dir (str): Output directory of the generated info file.
-        workers (int): Number of threads to be used.
-    """
-    indoor.create_indoor_info_file(
-        root_path,
-        info_prefix,
-        out_dir,
-        workers=workers,
-        num_points=num_points)
+# def sunrgbd_data_prep(root_path, info_prefix, out_dir, workers, num_points):
+#     """Prepare the info file for sunrgbd dataset.
+#
+#     Args:
+#         root_path (str): Path of dataset root.
+#         info_prefix (str): The prefix of info filenames.
+#         out_dir (str): Output directory of the generated info file.
+#         workers (int): Number of threads to be used.
+#     """
+#     indoor.create_indoor_info_file(
+#         root_path,
+#         info_prefix,
+#         out_dir,
+#         workers=workers,
+#         num_points=num_points)
 
 
 def waymo_data_prep(root_path,
@@ -241,7 +241,7 @@ if __name__ == '__main__':
     if args.dataset == 'kitti':
         kitti_data_prep(
             root_path=args.root_path,
-            info_prefix=args.extra_tag,
+            info_prefix=args.extra_tag,  # 表示新文件的名字
             version=args.version,
             out_dir=args.out_dir,
             with_plane=args.with_plane)
@@ -271,19 +271,19 @@ if __name__ == '__main__':
             dataset_name='NuScenesDataset',
             out_dir=args.out_dir,
             max_sweeps=args.max_sweeps)
-    elif args.dataset == 'lyft':
-        train_version = f'{args.version}-train'
-        lyft_data_prep(
-            root_path=args.root_path,
-            info_prefix=args.extra_tag,
-            version=train_version,
-            max_sweeps=args.max_sweeps)
-        test_version = f'{args.version}-test'
-        lyft_data_prep(
-            root_path=args.root_path,
-            info_prefix=args.extra_tag,
-            version=test_version,
-            max_sweeps=args.max_sweeps)
+    # elif args.dataset == 'lyft':
+    #     train_version = f'{args.version}-train'
+    #     lyft_data_prep(
+    #         root_path=args.root_path,
+    #         info_prefix=args.extra_tag,
+    #         version=train_version,
+    #         max_sweeps=args.max_sweeps)
+    #     test_version = f'{args.version}-test'
+    #     lyft_data_prep(
+    #         root_path=args.root_path,
+    #         info_prefix=args.extra_tag,
+    #         version=test_version,
+    #         max_sweeps=args.max_sweeps)
     elif args.dataset == 'waymo':
         waymo_data_prep(
             root_path=args.root_path,
@@ -292,22 +292,22 @@ if __name__ == '__main__':
             out_dir=args.out_dir,
             workers=args.workers,
             max_sweeps=args.max_sweeps)
-    elif args.dataset == 'scannet':
-        scannet_data_prep(
-            root_path=args.root_path,
-            info_prefix=args.extra_tag,
-            out_dir=args.out_dir,
-            workers=args.workers)
-    elif args.dataset == 's3dis':
-        s3dis_data_prep(
-            root_path=args.root_path,
-            info_prefix=args.extra_tag,
-            out_dir=args.out_dir,
-            workers=args.workers)
-    elif args.dataset == 'sunrgbd':
-        sunrgbd_data_prep(
-            root_path=args.root_path,
-            info_prefix=args.extra_tag,
-            num_points=args.num_points,
-            out_dir=args.out_dir,
-            workers=args.workers)
+    # elif args.dataset == 'scannet':
+    #     scannet_data_prep(
+    #         root_path=args.root_path,
+    #         info_prefix=args.extra_tag,
+    #         out_dir=args.out_dir,
+    #         workers=args.workers)
+    # elif args.dataset == 's3dis':
+    #     s3dis_data_prep(
+    #         root_path=args.root_path,
+    #         info_prefix=args.extra_tag,
+    #         out_dir=args.out_dir,
+    #         workers=args.workers)
+    # elif args.dataset == 'sunrgbd':
+    #     sunrgbd_data_prep(
+    #         root_path=args.root_path,
+    #         info_prefix=args.extra_tag,
+    #         num_points=args.num_points,
+    #         out_dir=args.out_dir,
+    #         workers=args.workers)
