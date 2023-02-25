88cc4164194fb2d4652a0076b7f1d3ce89ead8cb
rui <919304134@qq.com>
2023-02-25 16:02:09 +0800

    version:23/02/23,成功完成点云特征提取的注意力机制添加

--- mmdet3d/models/voxel_encoders/pillar_encoder.py
+++ mmdet3d/models/voxel_encoders/pillar_encoder.py
@@ -1,16 +1,16 @@
 # Copyright (c) OpenMMLab. All rights reserved.
 import torch
 from mmcv.cnn import build_norm_layer
+from mmcv.utils.registry import build_from_cfg
 from mmcv.ops import DynamicScatter
-from mmcv.runner import force_fp32, auto_fp16
+from mmcv.runner import force_fp32, auto_fp16, BaseModule
 from torch import nn
 
 from ..builder import VOXEL_ENCODERS
-from .utils import PFNLayer, get_paddings_indicator
-from torch.nn.modules import activation
-from torch.nn.modules.activation import MultiheadAttention
-from ..model_utils.transformer import PillarAttention
-import torch.nn.functional as F
+from .utils import PFNLayer, get_paddings_indicator, PFNLayer_nomax
+from mmcv.cnn.bricks.transformer import (build_attention, build_positional_encoding,
+                                         build_feedforward_network)
+from mmdet3d.models.utils.mlp import MLP
 
 @VOXEL_ENCODERS.register_module()
 class PillarFeatureNet(nn.Module):
@@ -242,11 +242,9 @@ class DynamicPillarFeatureNet(PillarFeatureNet):
         self.num_pfn = len(pfn_layers)  # 1
         self.pfn_layers = nn.ModuleList(pfn_layers)
         # DynamicScatter(voxel_size=[0.16, 0.16, 4], point_cloud_range=[0, -39.68, -3, 69.12, 39.68, 1], average_points=False)
-        self.pfn_scatter = DynamicScatter(voxel_size, point_cloud_range,
-                                          (mode != 'max'))
+        self.pfn_scatter = DynamicScatter(voxel_size, point_cloud_range, reduce='max')
         # DynamicScatter(voxel_size=[0.16, 0.16, 4], point_cloud_range=[0, -39.68, -3, 69.12, 39.68, 1], average_points=True)
-        self.cluster_scatter = DynamicScatter(
-            voxel_size, point_cloud_range, average_points=True)
+        self.cluster_scatter = DynamicScatter(voxel_size, point_cloud_range, reduce='mean')
 
     def map_voxel_center_to_point(self, pts_coors, voxel_mean, voxel_coors):
         """Map the centers of voxels to its corresponding points.
@@ -296,7 +294,7 @@ class DynamicPillarFeatureNet(PillarFeatureNet):
         Args:
             features (torch.Tensor): Point features or raw points in shape
                 (N, M, C). (4030,4)原始点云坐标和r
-            coors (torch.Tensor): Coordinates of each voxel (4030,4)
+            coors (torch.Tensor): Coordinates of each voxel (4030,4) bs,x,y,z
 
         Returns:
             torch.Tensor: Features of pillars.
@@ -346,72 +344,11 @@ class DynamicPillarFeatureNet(PillarFeatureNet):
         return voxel_feats, voxel_coors  # 提取后的体素特征和体素坐标
 
 
-
-# 定义新的PFN层，不做最大值操作，保留每个Voxel的全部特征点
-class PFNLayer_nomax(nn.Module):
-    def __init__(self,
-                 in_channels,  # 10
-                 out_channels,  # 64
-                 norm_cfg=dict(type='BN1d', eps=1e-3, momentum=0.01),  # {'type':'BN1d', 'eps':0.001, 'momentum':0.01}
-                 last_layer=False,  # True
-                 mode='max'):  # max
-        super(PFNLayer_nomax, self).__init__()
-        self.fp16_enabled = False
-        self.name = 'PFNLayer_nomax'
-        self.last_vfe = last_layer
-        if not self.last_vfe:
-            out_channels = out_channels // 2
-        self.units = out_channels  # 64
-        # BatchNorm1d(64, eps=0.001, momentum=0.01, affine=True, track_running_stats=True)
-        self.norm = build_norm_layer(norm_cfg, self.units)[1]
-        # Linear(in_features=10, out_features=64, bias=False)
-        self.linear = nn.Linear(in_channels, self.units, bias=False)
-
-        assert mode in ['max', 'avg']
-        self.mode = mode
-
-    @auto_fp16(apply_to=('inputs'), out_fp32=True)
-    def forward(self, inputs, num_voxels=None, aligned_distance=None):
-        '''
-
-        Args:
-            inputs: (N, M, C)
-            num_voxels:
-
-        Returns:
-            torch.Tensor: Features of Pillars.
-        '''
-        x = self.linear(inputs)
-        x = self.norm(x.permute(0, 2, 1).contiguous()).permute(0, 2,
-                                                               1).contiguous()
-        x = F.leaky_relu(x)
-
-        if self.mode == 'max':
-            if aligned_distance is not None:
-                x = x.mul(aligned_distance.unsqueeze(-1))
-            x_max = torch.max(x, dim=1, keepdim=True)[0]
-        elif self.mode == 'avg':
-            if aligned_distance is not None:
-                x = x.mul(aligned_distance.unsqueeze(-1))
-            x_max = x.sum(
-                dim=1, keepdim=True) / num_voxels.type_as(inputs).view(
-                -1, 1, 1)
-
-        if self.last_vfe:
-            return x
-        else:
-            x_repeat = x_max.repeat(1, inputs.shape[1], 1)
-            x_concatenated = torch.cat([x, x_repeat], dim=2)
-            return x_concatenated
-
-
 @VOXEL_ENCODERS.register_module()
-class AttnPFN(nn.Module):
+class AttnPFN(BaseModule):
     def __init__(self,
                  in_channels=4,
                  feat_channels=(64,),
-                 embed_dims=3,
-                 num_heads=3,
                  with_distance=False,
                  with_cluster_center=True,
                  with_voxel_center=True,
@@ -419,10 +356,37 @@ class AttnPFN(nn.Module):
                  point_cloud_range=(0, -40, -3, 70.4, 40, 1),
                  norm_cfg=dict(type='BN1d', eps=1e-3, momentum=0.01),
                  mode='max',
-                 legacy=True):
+                 legacy=True,
+                 multihead_attention=dict(
+                     type='MultiheadAttention',
+                     embed_dims=64,
+                     num_heads=8,
+                     attn_drop=0.,
+                     proj_drop=0.,
+                     dropout_layer=dict(type='Dropout', drop_prob=0.),
+                     init_cfg=None,
+                     batch_first=True),
+                 pos_encoding=dict(
+                     type='ConvBNPositionalEncoding',
+                     input_channel=64,
+                     num_pos_feats=64),
+                 # mlp
+                 mlp_in_channel=10,
+                 mlp_conv_channels=(64,),
+                 mlp_conv_cfg=dict(type='Conv1d'),
+                 mlp_norm_cfg=dict(type='BN1d'),
+                 mlp_act_cfg=dict(type='LeakyReLU'),
+                 mlp_bias=False,
+                 # # ffn
+                 # ffn=dict(
+                 #     type='FFN',
+                 #     embed_dims=10,
+                 #     feedforward_channels=64,
+                 #     num_fcs=2,
+                 #     ffn_drop=0.1)
+                 ):
+
         super(AttnPFN, self).__init__()
-        self.embed_dims = embed_dims
-        self.num_heads = num_heads
         assert len(feat_channels) > 0  # feat_channels=[64] from pp
         self.legacy = legacy
         if with_cluster_center:
@@ -434,14 +398,14 @@ class AttnPFN(nn.Module):
         self._with_distance = with_distance  # False
         self._with_cluster_center = with_cluster_center  # True
         self._with_voxel_center = with_voxel_center  # True
-        self.fp16_enabled = False
-        # Create PillarFeatureNet layers
+        self.fp16_enabled = False  # 可使用混合精度训练
+        # PFN
         self.in_channels = in_channels  # 10
         feat_channels = [in_channels] + list(feat_channels)  # [10,64]
         pfn_layers = []
         for i in range(len(feat_channels) - 1):  # i=0,pfn中只有一层网络
             in_filters = feat_channels[i]  # 10
-            out_filters = feat_channels[i + 1]  # 64
+            out_filters = feat_channels[i+1]  # 64
             if i < len(feat_channels) - 2:  # False
                 last_layer = False
             else:
@@ -454,6 +418,16 @@ class AttnPFN(nn.Module):
                     last_layer=last_layer,  # True
                     mode=mode))  # max
         self.pfn_layers = nn.ModuleList(pfn_layers)  # fpn层加入模块中
+        # MLP
+        self.mlp = MLP(in_channel=mlp_in_channel, conv_channels=mlp_conv_channels,
+                       conv_cfg=mlp_conv_cfg, norm_cfg=mlp_norm_cfg, act_cfg=mlp_act_cfg,
+                       bias=mlp_bias)
+        # pos_encoding
+        self.pos_encoding = build_positional_encoding(pos_encoding)
+        # multihead_attention
+        self.multihead_attention = build_attention(multihead_attention)
+        # # ffn
+        # self.ffn = build_feedforward_network(ffn)
 
         # Need pillar (voxel) size and x/y offset in order to calculate offset  计算体素中点的偏移量
         self.vx = voxel_size[0]  # 0.16
@@ -463,6 +437,8 @@ class AttnPFN(nn.Module):
         self.y_offset = self.vy / 2 + point_cloud_range[1]  # -39.6
         self.z_offset = self.vz / 2 + point_cloud_range[2]  # -1.0
         self.point_cloud_range = point_cloud_range
+
+        self.mlp2 = MLP(64, [64, 64], act_cfg=mlp_act_cfg, bias=False)
 
 
     @force_fp32(out_fp16=True)
@@ -478,13 +454,8 @@ class AttnPFN(nn.Module):
                 Returns:
                     torch.Tensor: Features of pillars.
                 """
-        # 通过注意力模块计算每个点的最大注意力分数
-        attn_block = PillarAttention(self.embed_dims, self.num_heads)
-        features_attn = attn_block(features[:,:,:3])
-        # TODO：将相关性分数做归一化，控制值在0-1之间
-        max_features_attn = torch.max(features_attn, -1, keepdim=True)[0]  # 作为每个点的相关性分数
 
-        # 用PFN层提取特征
+        # ---------------------------------------------step1 特征增强至10维---------------------------------------------
         # 将传入的特征tensor放入list
         features_ls = [features]
         # Find distance of x, y, and z from cluster center
@@ -539,19 +510,149 @@ class AttnPFN(nn.Module):
         voxel_count = features.shape[1]  # 每个体素中的点数
         mask = get_paddings_indicator(num_points, voxel_count, axis=0)
         mask = torch.unsqueeze(mask, -1).type_as(features)
-        features *= mask
+        features *= mask  # 增强后的10维特征
 
-        # 抽取每个体素内的特征
+        # ---------------------------------------step2 pfn-------------------------------------
+        pfn_features = features
         for pfn in self.pfn_layers:
             # 将特征放入FPN中提取并maxpooling，channels: in10 out64
-            features = pfn(features, num_points)  # 32*10->32*64->1*64
+            pfn_features = pfn(pfn_features, num_points)
+        fm = torch.max(pfn_features, dim=1, keepdim=True)[0]
 
-        # 每个点的最大注意力分数与PFN提取的点特征相乘再除总注意力分数（正则化）
-        fa = (max_features_attn * features) / max_features_attn.sum(dim=1, keepdim=True)
-        fa = torch.max(fa, dim=1, keepdim=True)[0]
-        # 经过FPN后再做max的特征
-        fm = torch.max(features, dim=1, keepdim=True)[0]
+        # ---------------------------------------step3 pos_encoding-------------------------------------
+        # features = features.transpose(1, 2)
+        # features = self.mlp(features)
+        # features = features.transpose(1, 2)
+        pos_encoding = self.pos_encoding(features)
+        pos_encoding = pos_encoding.transpose(1, 2)
+
+        # ---------------------------------------step4 multihead_attention-------------------------------------
+        multihead_attention = self.multihead_attention(query=features, query_pos=pos_encoding, key_pos=pos_encoding)
+        # multihead_attention = self.ffn(multihead_attention)  # ffn
+        # mlp
+        multihead_attention = self.mlp(multihead_attention.transpose(1, 2)).transpose(1, 2)
+        a = torch.sum(multihead_attention, dim=2, keepdim=True)
+        fa = (pfn_features * multihead_attention) / a
+        fa = torch.sum(fa, dim=1, keepdim=True)  # (97297,1,64)
+
         # 总特征
-        f = (fa + fm) / 2
+        f = ((fa + fm) / 2).transpose(1, 2)
+        f = self.mlp2(f).transpose(1, 2)
 
-        return f.squeeze(1)
+        return f.squeeze(1)  # (98,64)
+
+
+@VOXEL_ENCODERS.register_module()
+class DynamicAttnPFN(DynamicPillarFeatureNet):
+    def __init__(self,
+                 in_channels=4,
+                 feat_channels=(64,),
+                 with_distance=False,
+                 with_cluster_center=True,
+                 with_voxel_center=True,
+                 voxel_size=(0.2, 0.2, 4),
+                 point_cloud_range=(0, -40, -3, 70.4, 40, 1),
+                 norm_cfg=dict(type='BN1d', eps=1e-3, momentum=0.01),
+                 mode='max',
+                 legacy=True,
+                 multihead_attention=dict(
+                     type='MultiheadAttention',
+                     embed_dims=64,
+                     num_heads=8,
+                     attn_drop=0.,
+                     proj_drop=0.,
+                     dropout_layer=dict(type='Dropout', drop_prob=0.),
+                     init_cfg=None,
+                     batch_first=True),
+                 pos_encoding=dict(
+                     type='ConvBNPositionalEncoding',
+                     input_channel=64,
+                     num_pos_feats=64),
+                 mlp_in_channel=10,
+                 mlp_conv_channels=[64]
+                 ):
+        super(DynamicAttnPFN, self).__init__()
+        #mlp
+        self.mlp_in_channel = mlp_in_channel
+        self.mlp_conv_channels = mlp_conv_channels
+        # pos_encoding
+        self.pos_encoding = build_positional_encoding(pos_encoding)
+        # multihead_attention
+        self.multihead_attention = build_attention(multihead_attention)
+        # attention得到的注意力分数进行分配，并取sum
+        self.attention_scatter = DynamicScatter(voxel_size, point_cloud_range, reduce='sum')
+
+    @force_fp32(out_fp16=True)
+    def forward (self, features, coors):
+
+        # ----------------------------------------step1 特征增强至10维------------------------------------
+        features_ls = [features]  # 加入原始点云特征(4030,4) 4:(x,y,z,r)
+        # Find distance of x, y, and z from cluster center 计算点和聚类中心的距离
+        if self._with_cluster_center:
+            # 计算聚类中心
+            # coors:所有体素的坐标/所有点对应的体素坐标    mean_coors:含有points的体素坐标    voxel_mean：由points提取出的体素特征
+            voxel_mean, mean_coors = self.cluster_scatter(features, coors)  # (98,4) (98,4)
+            # points_mean:计算点的聚类中心
+            points_mean = self.map_voxel_center_to_point(
+                coors, voxel_mean, mean_coors)  # (4030,4)
+            # TODO: maybe also do cluster for reflectivity
+            # 计算点和点聚类中心的偏差
+            f_cluster = features[:, :3] - points_mean[:, :3]  # (4030,3)
+            features_ls.append(f_cluster)  # [(4030,4),(4030,3)]
+
+        # Find distance of x, y, and z from pillar center 计算点和体素中心的距离
+        if self._with_voxel_center:
+            f_center = features.new_zeros(size=(features.size(0), 3))  # (4030,3)
+            f_center[:, 0] = features[:, 0] - (
+                    coors[:, 3].type_as(features) * self.vx + self.x_offset)  # 点x方向的偏移
+            f_center[:, 1] = features[:, 1] - (
+                    coors[:, 2].type_as(features) * self.vy + self.y_offset)  # 点y方向的偏移
+            f_center[:, 2] = features[:, 2] - (
+                    coors[:, 1].type_as(features) * self.vz + self.z_offset)  # 点z方向的偏移
+            features_ls.append(f_center)  # f_center=(4030,3) features_ls=[(4030,4),(4030,3)]
+
+        if self._with_distance:
+            points_dist = torch.norm(features[:, :3], 2, 1, keepdim=True)
+            features_ls.append(points_dist)
+
+        # Combine together feature decorations
+        features = torch.cat(features_ls, dim=-1)  # (4030,10) include:点特征(原始坐标)，点与聚类中心距离，点和体素中心距离
+
+        # ----------------------------------------------step2 pfn-----------------------------------------------
+        pfn_features = features
+        for i, pfn in enumerate(self.pfn_layers):
+            # 在全局点云内提取点的特征，得到每个点的全局特征 10->64
+            point_feats = pfn(pfn_features)  # (4030,64)
+            # 由体素内的点特征做最大池化得到体素特征 voxel_feats:体素特征 voxel_coors:体素坐标
+            voxel_feats, voxel_coors = self.pfn_scatter(point_feats, coors)  # (98,64) (98,4)
+            if i != len(self.pfn_layers) - 1:
+                # need to concat voxel feats if it is not the last pfn
+                feat_per_point = self.map_voxel_center_to_point(
+                    coors, voxel_feats, voxel_coors)
+                pfn_features = torch.cat([point_feats, feat_per_point], dim=1)
+        fm = voxel_feats  # (98,64)
+
+        # -----------------------------------------------step3 pos_encoding-----------------------------------------
+        features = features.unsqueeze(0)  # 增加bs维度 (1,4030,10)
+        features = features.transpose(1, 2)
+        # build mlp
+        mlp = MLP(in_channel=self.mlp_in_channel, conv_channels=self.mlp_conv_channels).cuda()
+        features = mlp(features)
+        features = features.transpose(1, 2)  # (1,4030,64)
+        pos_encoding = self.pos_encoding(features)
+        pos_encoding = pos_encoding.transpose(1, 2)  # (1,4030,64)
+
+        # ------------------------------------------step4 multihead_attention-------------------------------------
+        multihead_attention = self.multihead_attention(query=features, query_pos=pos_encoding, key_pos=pos_encoding)
+        fa = features * multihead_attention  # (1,4030,64)
+        # mlp2 = MLP(32, 64).cuda()
+        # fa = mlp2(fa)
+        fa = fa.squeeze(0)  # (4030,64)
+        fa = self.attention_scatter(fa, coors)[0]  # (98,64)
+        # 总特征
+        f = (fm + fa)/2
+
+        return f, voxel_coors  # (98,64)
+
+
+
