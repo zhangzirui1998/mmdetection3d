1920edb032186ccf0423cbac03ab148137159a69
rui <919304134@qq.com>
2023-02-28 19:02:37 +0800

    add selfattention

--- mmdet3d/models/model_utils/__init__.py
+++ mmdet3d/models/model_utils/__init__.py
@@ -1,6 +1,6 @@
 # Copyright (c) OpenMMLab. All rights reserved.
 from .edge_fusion_module import EdgeFusionModule
-from .transformer import GroupFree3DMHA, PillarAttention
+from .transformer import GroupFree3DMHA, PillarAttention, SelfAttention
 from .vote_module import VoteModule
 
-__all__ = ['VoteModule', 'GroupFree3DMHA', 'EdgeFusionModule', 'PillarAttention']
+__all__ = ['VoteModule', 'GroupFree3DMHA', 'EdgeFusionModule', 'PillarAttention', 'SelfAttention']
