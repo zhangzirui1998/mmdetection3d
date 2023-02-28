e2c7bd3fd161d3dbf118a3e7bf368eeab6491592
rui <919304134@qq.com>
2023-02-28 19:01:34 +0800

    delete forward fp16

--- mmdet3d/models/necks/second_fpn.py
+++ mmdet3d/models/necks/second_fpn.py
@@ -75,7 +75,6 @@ class SECONDFPN(BaseModule):
                 dict(type='Constant', layer='NaiveSyncBatchNorm2d', val=1.0)
             ]
 
-    @auto_fp16()
     def forward(self, x):
         """Forward function.
 
