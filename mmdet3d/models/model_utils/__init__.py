451abc93e759a2177dd7bc05a33a2ca798a0aad6
rui <919304134@qq.com>
2023-03-03 21:27:06 +0800

    pillar_attention->dynamic_attention

--- mmdet3d/models/model_utils/__init__.py
+++ mmdet3d/models/model_utils/__init__.py
@@ -1,6 +1,6 @@
 # Copyright (c) OpenMMLab. All rights reserved.
 from .edge_fusion_module import EdgeFusionModule
-from .transformer import GroupFree3DMHA, PillarAttention, SelfAttention
+from .transformer import GroupFree3DMHA, SelfAttention, DynamicSelfAttention
 from .vote_module import VoteModule
 
-__all__ = ['VoteModule', 'GroupFree3DMHA', 'EdgeFusionModule', 'PillarAttention', 'SelfAttention']
+__all__ = ['VoteModule', 'GroupFree3DMHA', 'EdgeFusionModule', 'DynamicSelfAttention', 'SelfAttention']
